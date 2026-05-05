898fbe5544ce11137f70c851c8ce2eae84345235
Blackgamerz <105124395+BlackgamerzVN@users.noreply.github.com>
2026-05-06 05:24:46 +0700

    Revert "Stage 2: narrow catch in performFireAction from Throwable to Exception"
    This reverts commit 319c5e6f2c653787beb65d289237c8fd94c54dd6.

--- src/main/java/com/blackgamerz/jmteg/recruitcompat/RecruitRangedGunnerAttackGoal.java
+++ src/main/java/com/blackgamerz/jmteg/recruitcompat/RecruitRangedGunnerAttackGoal.java
@@ -1125,7 +1125,7 @@ public class RecruitRangedGunnerAttackGoal extends Goal {
             if (mob.isUsingItem()) mob.stopUsingItem();
             mob.startUsingItem(InteractionHand.MAIN_HAND);
             mob.releaseUsingItem();
-        } catch (Exception t) {
+        } catch (Throwable t) {
             LOGGER.debug("performFireAction failed for {}: {}", mob, t.toString());
         }
     }
